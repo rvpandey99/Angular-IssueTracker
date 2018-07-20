40bff1220d51f91b69b5ec56f8c740478f2e2fa4
rvpandey99 <rvpandey99@gmail.com>
2018-07-20 06:18:56 +0530

    Created AddNew UI

--- src/app/addnew/addnew.component.ts
+++ src/app/addnew/addnew.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class AddnewComponent implements OnInit {
 
   constructor() { }
+  private date = new Date();
+  createdDate = this.date;
+  issueId = (this.date.getTime()).toString();
 
   ngOnInit() {
   }
